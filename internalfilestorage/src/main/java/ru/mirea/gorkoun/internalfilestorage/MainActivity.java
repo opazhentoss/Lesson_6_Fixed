e5faef4f1f409552709d2948fbb1067a48a7e509
evgen <gorkoune@gmail.com>
2023-05-22 21:53:26 +0300

    update

---
+++ internalfilestorage/src/main/java/ru/mirea/gorkoun/internalfilestorage/MainActivity.java
@@ -0,0 +1,77 @@
+package ru.mirea.gorkoun.internalfilestorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    public TextView tv;
+    public EditText et;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private String fileName = "mirea.txt";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textView);
+        et = findViewById(R.id.inputInfo);
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                tv.post(new Runnable() {
+                    public void run() {
+                        tv.setText(getTextFromFile());
+                    }
+                });
+            }
+        }).start();
+    }
+    // открытие файла
+    public String getTextFromFile() {
+        FileInputStream fin = null;
+        try {
+            fin = openFileInput(fileName);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            Log.d(LOG_TAG, text);
+            return text;
+        } catch (IOException ex) {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        } finally {
+            try {
+                if (fin != null)
+                    fin.close();
+            } catch (IOException ex) {
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        return null;
+    }
+    public void writeTextInFile(View view){
+        String string = et.getText().toString();
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
